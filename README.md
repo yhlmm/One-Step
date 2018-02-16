b72a604a21e1f75d7269fceb02f9ee0f18fdc282
grayondream <grayondream@outlook.com>
2018-02-16 23:25:12 +0800

    add some test code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # One-Step
 First step,just attempt
+
+add a function in main.cpp to test the git,
--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@
 #include <iostream>
 using namespace std;
 
+void func(int n)
+{
+    cout<<"just for test!";
+}
+
 int main(int argc, char **argv)
 {
     cout<<"git push code to other's code respository\n";
